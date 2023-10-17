0f4a291cfb0ac077352eb042319655c9d548b87d
Jesus Cambero <121453314+tempoDev@users.noreply.github.com>
2023-10-17 11:24:59 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -106,9 +106,9 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
   if (error.name === 'CastError'){
-    return response.status(400).send({ error: 'malformatted id'})
+    return response.status(400).send({ message: 'malformatted id'})
   } else if (error.name === 'ValidationError'){
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({message: error.message})
   }
 
 }
